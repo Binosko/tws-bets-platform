0f94a89c3a48a013a4ced40432d7120a45b1e72f
Binosko <abab86620@gmail.com>
2025-12-10 14:57:35 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone', // This tells Vercel it's a Next.js app
-}
-
-module.exports = nextConfig
