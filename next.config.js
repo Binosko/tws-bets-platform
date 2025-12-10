48ae08e691f1d61a785488afa728f1acd2456679
Binosko <abab86620@gmail.com>
2025-12-10 14:52:52 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', // This tells Vercel it's a Next.js app
+}
+
+module.exports = nextConfig
